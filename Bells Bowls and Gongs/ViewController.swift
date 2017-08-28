aabc700fd6592197ef3714b8ad8a4e5b244052a4
David Lavallee <d.lavallee@charter.net>
2017-08-28 19:36:31 -0400

    added sound setups

--- Bells Bowls and Gongs/ViewController.swift
+++ Bells Bowls and Gongs/ViewController.swift
@@ -9,11 +9,58 @@
 import UIKit
 import AVFoundation
 
+
+var playBowlA = AVAudioPlayer()
+var playBowlB = AVAudioPlayer()
+var playBurmese = AVAudioPlayer()
+var playChinese = AVAudioPlayer()
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //setup wav files to play
+        ////////////////////////////
+        
+        //BowlA
+        do {
+            playBowlA = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path (forResource: "bowla", ofType: "wav")!))
+            playBowlA.prepareToPlay()
+        }
+        catch {
+            print(error)
+        }
+        
+        //BowlB
+        do {
+            playBowlB = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path (forResource: "bowla", ofType: "wav")!))
+            playBowlB.prepareToPlay()
+        }
+        catch {
+            print(error)
+        }
+        
+        //Burmese
+        do {
+            playBurmese = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path (forResource: "bowla", ofType: "wav")!))
+            playBurmese.prepareToPlay()
+        }
+        catch {
+            print(error)
+        }
+        
+        //Chinese
+        do {
+            playChinese = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path (forResource: "bowla", ofType: "wav")!))
+            playChinese.prepareToPlay()
+        }
+        catch {
+            print(error)
+        }
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +68,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    
+    
 }
 
